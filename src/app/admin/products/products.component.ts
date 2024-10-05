9829ce0d24b28e3aceac1ffd9eae693285a554e1
dell <dell@DESKTOP-4P8U8AR>
2024-10-05 02:55:51 +0100

    Product List, New Product (Vue-Component) and Logique

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  isSidePanelVisible: boolean= true;
+  productObj: any = {
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createDate": new Date(),
+    "deliveryTimeSpan": "",
+    "categoryId": 0,
+    "productImageUrl": ""
+  }
+
+  openSidePanel() {
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel() {
+    this.isSidePanelVisible = false;
+  }
 }
