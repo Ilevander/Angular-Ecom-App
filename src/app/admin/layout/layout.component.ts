01c2d503ecf0b1f808deb3f18349e71012f77a32
dell <dell@DESKTOP-4P8U8AR>
2024-10-12 16:26:26 +0100

    re-structuring the Layout

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,17 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {ProductService} from '../../services/product/product.service';
+import {LoginService} from '../../services/login/login.service';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
 
+export class LayoutComponent {
+  searchText: string = '';
+
+  constructor(private loginService: LoginService) { }
+
+  onFilter() {
+    this.loginService.searchBox.next(this.searchText);
+  }
+
+  onInput(event: any) {
+    if (event.target.value.trim() === '') {
+      this.onFilter();
+    }
+  }
 }
